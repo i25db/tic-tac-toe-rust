d11f50c13598cefa19ddfcf3c7ca0cd5fcfbe5ef
i25db <i25.db@outlook.com>
2021-11-30 03:51:50 +0000

    Implement game prompt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
 use ttt_rust::TicTacToe;
+use std::io::{stdin, BufRead};
 
 fn main() {
     let ttt = TicTacToe::new();
 
     print_board(ttt);
+}
+
+fn prompt_for_move(player: char) -> (i8, i8) {
+    print!("Play '{}' pick location (#,#)>", player);
+    
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let split = input.split(',').collect::<Vec<&str>>();
+
+    if split.len() != 2 {
+        println!("Please input two numbers for coordinates (#,#)");
+
+        prompt_for_move(player)
+    } else {
+        let x = split[0].parse::<i8>();
+        let y = split[1].parse::<i8>();
+
+        match x {
+            Ok(x) => match y {
+                Ok (y) => (x, y),
+                Err(e) => {
+                    println!("Please input two numbers for coordinates (#,#)");
+
+                    prompt_for_move(player)
+                }
+            },
+            Err(e) => {
+                println!("Please input two numbers for coordinates (#,#)");
+
+                prompt_for_move(player)
+
+            }
+        }
+    }
 }
 
 fn print_board(ttt: TicTacToe) {
