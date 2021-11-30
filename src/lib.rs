c9e43644c1f10c3c1d2a8d73dcf6e9b7179bfa1e
i25db <i25.db@outlook.com>
2021-11-30 10:33:39 +0000

    Add logic to check game state

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
+use std::array::IntoIter;
+
 pub struct TicTacToe {
-    pub board : [char; 9]
+    board : [char; 9]
 }
 
 impl TicTacToe {
     pub fn new() -> TicTacToe {
         TicTacToe {
-            board: [' '; 9]
+            board: ['_'; 9]
         }
     }
+
+    pub fn into_iter(&self) -> IntoIter<char, 9_usize> {
+        self.board.into_iter()
+    } 
 
     pub fn at(&self, x: i8, y: i8) -> Option<char> {
         // Bounds check
@@ -23,12 +29,80 @@ impl TicTacToe {
         match self.at(x, y) {
             None => Some(String::from("Position out of bounds")),
             Some(ch) => {
-                if ch != ' ' {
+                if ch != '_' {
                     Some(String::from("Position not empty"))
                 } else {
+                    self.board[(x*y-1) as usize] = c;
                     None
                 }
             }
+        }
+    }
+
+    // Returns 'x' or 'o' if one or the other has won '_' for game
+    // not over and None if its a draw
+    pub fn is_game_over(&self) -> Option<char> {
+        // Each element is a Row, Column or Diagonal and represents
+        // how many x's and how many o's are in it
+        // 0-2: rows
+        // 3-5: columns
+        // 6: 1,1 diagonal
+        // 7: 3,1 diagonal
+        let mut rcd = [(0, 0); 8];
+        let mut empty_positions = 9;
+
+        for (i, c) in self.board.into_iter().enumerate() {
+            let x = (i % 3) + 1;
+            let y = ((i / 3) % 3) + 1;
+
+            match c {
+                'x' => {
+                    empty_positions -= 1;
+
+                    // increment x count for row and column
+                    rcd[x].0 += 1;
+                    rcd[y+3].0 += 1;
+
+                    // i is on diagonal 1,1 when i % 4 == 0
+                    if i % 4 == 0 {
+                        rcd[6].0 += 1;
+                    }
+                    // position is on diagonal 3,1 when x + y == 4
+                    // don't count 2,2 twice so we else if
+                    // besides 2,2 diagnoals share no positions
+                    else if x + y == 4 {
+                        rcd[7].0 += 1;
+                    }
+
+                    if rcd[x].0 == 3 || rcd[y+3].0 == 3 || rcd[6].0 == 3 || rcd[7].0 == 3 {
+                        return Some('x');
+                    }
+                },
+                'o' => {
+                    empty_positions -= 1;
+
+                    // increment y count for row and column
+                    rcd[x].1 += 1;
+                    rcd[y+3].1 += 1;
+
+                    if i % 4 == 0 {
+                        rcd[6].1 += 1;
+                    } else if x + y == 4 {
+                        rcd[7].1 += 1;
+                    }
+
+                    if rcd[x].1 == 3 || rcd[y+3].1 == 3 || rcd[6].1 == 3 || rcd[7].1 == 3 {
+                        return Some('o');
+                    }
+                },
+                _ => ()
+            };
+        }
+
+        if empty_positions == 0 {
+            None
+        } else {
+            Some('_')
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use ttt_rust::TicTacToe;
-use std::io::{stdin, BufRead};
+use std::io::stdin;
 
 fn main() {
-    let ttt = TicTacToe::new();
+    let mut ttt = TicTacToe::new();
+    
+    ttt.set(1, 1, 'o');
+    ttt.set(2, 1, 'x');
+    ttt.set(3, 1, 'o');
+    print_board(&ttt);
 
-    print_board(ttt);
+    println!("Winner is: {}", ttt.is_game_over().unwrap());
 }
 
 fn prompt_for_move(player: char) -> (i8, i8) {
-    print!("Play '{}' pick location (#,#)>", player);
+    print!("Player '{}' pick location (#,#)>", player);
     
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
@@ -26,13 +31,13 @@ fn prompt_for_move(player: char) -> (i8, i8) {
         match x {
             Ok(x) => match y {
                 Ok (y) => (x, y),
-                Err(e) => {
+                Err(_) => {
                     println!("Please input two numbers for coordinates (#,#)");
 
                     prompt_for_move(player)
                 }
             },
-            Err(e) => {
+            Err(_) => {
                 println!("Please input two numbers for coordinates (#,#)");
 
                 prompt_for_move(player)
@@ -42,11 +47,11 @@ fn prompt_for_move(player: char) -> (i8, i8) {
     }
 }
 
-fn print_board(ttt: TicTacToe) {
-    for (i, c) in ttt.board.into_iter().enumerate() {
+fn print_board(ttt: &TicTacToe) {
+    for (i, c) in ttt.into_iter().enumerate() {
         print!("{} ", c);
 
-        if i % 3 == 0 {
+        if (i+1) % 3 == 0 {
             println!();
         }
     }
